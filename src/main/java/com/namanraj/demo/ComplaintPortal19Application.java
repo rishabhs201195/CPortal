0a5ec71c5386f6a458ed6d661d0178c43d5eb7b6
Namanraj Varshney <naman11396@gmail.com>
2019-04-23 17:09:11 +0530

    Add files via upload

---
+++ src/main/java/com/namanraj/demo/ComplaintPortal19Application.java
@@ -0,0 +1,13 @@
+package com.namanraj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintPortal19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintPortal19Application.class, args);
+	}
+
+}
